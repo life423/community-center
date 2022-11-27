3dae12e827b6b881b6591f3b906f5c9d4eab21ef
Andrew Clark <drewboy879@gmail.com>
2022-11-27 12:21:57 -0600

    progress on responsive navbar with JS

--- script.js
+++ script.js
@@ -2,7 +2,28 @@ const header = document.querySelector('header');
 const menu = document.querySelector('.menu__icon');
 const width = window.innerWidth;
 const height = window.innerHeight;
-if (width <= 600 ){
-    menu.classList.remove('hide');
 
-}
+//   if (width <= 600) {
+//     menu.classList.toggle('hide');
+//     console.log('fired');
+//   }
+
+window.addEventListener('load', e => {
+  e.preventDefault();
+
+  if (width <= 600) {
+    menu.classList.toggle('hide');
+    console.log('fired');
+  }
+});
+
+onresize = e => {
+  e.preventDefault();
+  const width = e.target.innerWidth;
+
+  if (width <= 600) {
+    menu.classList.remove('hide');
+  } else if (width >= 600) {
+    menu.classList.add('hide');
+  }
+};
