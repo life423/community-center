eede6cf5521a7df6460cc1f63e2a113d5f4165ed
Andrew Clark <drewboy879@gmail.com>
2022-11-27 11:29:47 -0600

    putting pieces together for navbar

---
+++ script.js
@@ -0,0 +1,8 @@
+const header = document.querySelector('header');
+const menu = document.querySelector('.menu__icon');
+const width = window.innerWidth;
+const height = window.innerHeight;
+if (width <= 600 ){
+    menu.classList.remove('hide');
+
+}
