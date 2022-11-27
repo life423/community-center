3428eddacb653efd679906d53cd47bce83764cd1
Andrew Clark <drewboy879@gmail.com>
2022-11-27 15:06:26 -0600

    adding flexbox to navbar

--- script.js
+++ script.js
@@ -7,9 +7,11 @@ const height = window.innerHeight;
 
 window.addEventListener('load', e => {
   e.preventDefault();
-
   if (width <= 600) {
     menu.classList.toggle('hide');
+    navItems.forEach(item => {
+      item.classList.toggle('hide');
+    });
   }
 });
 
@@ -20,16 +22,25 @@ onresize = e => {
   if (width <= 600) {
     menu.classList.remove('hide');
     navItems.forEach(item => {
-        item.classList.add('hide');
-    });  
-    
+      item.classList.add('hide');
+    });
   } else if (width >= 600) {
     menu.classList.add('hide');
     navItems.forEach(item => {
       item.classList.remove('hide');
-    });  
-
-
-
+    });
   }
 };
+
+menu.addEventListener('click', e => {
+  e.preventDefault();
+  navItems.forEach(item => {
+    item.classList.toggle('hide');
+  });
+  console.log('clicked');
+});
+
+
+// @media only screen and (max-width: 621px) {
+//         display: none;
+//     }
