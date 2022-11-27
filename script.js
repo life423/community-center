03950f0cee2343bc2c8f33b2b493f6bc068a1543
Andrew Clark <drewboy879@gmail.com>
2022-11-27 13:42:52 -0600

    progress more on navbar

--- script.js
+++ script.js
@@ -1,19 +1,15 @@
 const header = document.querySelector('header');
 const menu = document.querySelector('.menu__icon');
+const nav = document.querySelector('nav');
+
 const width = window.innerWidth;
 const height = window.innerHeight;
-
-//   if (width <= 600) {
-//     menu.classList.toggle('hide');
-//     console.log('fired');
-//   }
 
 window.addEventListener('load', e => {
   e.preventDefault();
 
   if (width <= 600) {
     menu.classList.toggle('hide');
-    console.log('fired');
   }
 });
 
