b0904f7b0108221aab9c3cbbf1edac587742a8c1
Andrew Clark <drew@drewclark.io>
2023-08-15 06:51:56 -0500

    Refactor menu toggle and navigation item hiding logic
    The commit refactors the code related to toggling the menu and hiding navigation items. It simplifies the logic by using arrow functions and removes unnecessary lines of code. The `hideNavItems` function now uses a single line to add the "hide" class to each navigation item, while `showNavItems` does the opposite. The `close` function is also simplified by calling `toggleMenu`, setting the height of the nav and header elements, and hiding navigation items. Additionally, two new functions are introduced: `adjustHeaderAndNav` adjusts the height of the header based on screen width, and `hideMenu` hides both menu icons and navigation items. Event listeners for window load and resize events call these functions accordingly. Finally, event listeners for menu icon clicks toggle classes, set heights, and show/hide navigation items; while an event listener for close icon click calls the `close` function.
    
    Note: This commit message assumes that all changes made in this diff are significant enough to warrant a commit message.

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-//need to reload page on rotate
-
 const header = document.querySelector(".header");
 const nav = document.querySelector(".nav");
-
 const icon = document.querySelector(".icon");
 const menuIcon = document.querySelector(".icon__menu");
-const navItems = document.querySelectorAll(".nav__item");
 const closeIcon = document.querySelector(".icon__close");
+const navItems = document.querySelectorAll(".nav__item");
 const navLinks = document.querySelectorAll(".nav__link");
+
 const width = window.innerWidth;
 const height = window.innerHeight;
 
@@ -17,100 +15,59 @@ const toggleMenu = () => {
 };
 
 const hideNavItems = () => {
-  navItems.forEach((item) => {
-    item.classList.add("hide");
-  });
+  navItems.forEach((item) => item.classList.add("hide"));
 };
 
 const showNavItems = () => {
-  navItems.forEach((item) => {
-    item.classList.remove("hide");
-  });
+  navItems.forEach((item) => item.classList.remove("hide"));
 };
 
-const showMenu = () => {
-  menuIcon.classList.remove("hide");
+const close = () => {
+  toggleMenu();
+  nav.style.height = "0%";
+  header.style.height = "14vh";
+  hideNavItems();
 };
 
-console.log("load fired");
-window.addEventListener("load", (e) => {
-  e.preventDefault();
-
+const adjustHeaderAndNav = () => {
   if (width <= 600) {
     header.style.height = "14vh";
-    menuIcon.classList.remove("hide");
-    icon.classList.remove("hide");
-    navItems.forEach((item) => {
-      item.classList.add("hide");
-    });
+    hideNavItems();
+    showMenu();
   } else if (width > height && width <= 1200) {
-    console.log("tablet-landscape load");
-    menuIcon.classList.add("hide");
     header.style.height = "18vh";
     showNavItems();
   } else if (width >= 600) {
     header.style.height = "10vh";
-    menuIcon.classList.add("hide");
-    icon.classList.add("hide");
-    navItems.forEach((item) => {
-      item.classList.remove("hide");
-    });
+    hideNavItems();
+    hideMenu();
   }
-});
-
-const close = () => {
-  closeIcon.classList.add("hide");
-  menuIcon.classList.remove("hide");
-  nav.style.height = "0%";
-  header.style.height = "14vh";
-  navItems.forEach((item) => {
-    item.classList.add("hide");
-  });
 };
 
-onresize = (e) => {
-  if (closeIcon.classList.contains("hide") === false) {
+const hideMenu = () => {
+  menuIcon.classList.add("hide");
+  icon.classList.add("hide");
+  hideNavItems();
+};
+
+window.addEventListener("load", () => {
+  adjustHeaderAndNav();
+});
+
+window.addEventListener("resize", () => {
+  if (!closeIcon.classList.contains("hide")) {
     close();
   }
-
-  e.preventDefault();
-
-  const width = e.target.innerWidth;
-  const height = e.target.innerHeight;
-  if (width <= 600) {
-    ("fired less than 600px");
-    hideNavItems();
-    showMenu();
-  } else if (width > height && width <= 1200) {
-    console.log("tablet-landscape resize");
-    menuIcon.classList.add("hide");
-    header.style.height = "18vh";
-    showNavItems();
-  } else if (width >= 600) {
-    ("fired more than 600px");
-    menuIcon.classList.add("hide");
-    showNavItems();
-  }
-};
-
-menuIcon.addEventListener("click", (e) => {
-  e.preventDefault();
-  menuIcon.classList.toggle("hide");
-  closeIcon.classList.toggle("hide");
-  nav.style.height = "60%";
-  header.style.height = "80vh";
-
-  navItems.forEach((item) => {
-    item.classList.remove("hide");
-  });
+  adjustHeaderAndNav();
 });
 
-closeIcon.addEventListener("click", (e) => {
-  e.preventDefault();
+menuIcon.addEventListener("click", () => {
   toggleMenu();
-  nav.style.height = "0%";
-  header.style.height = "14vh";
-  navItems.forEach((item) => {
-    item.classList.add("hide");
-  });
+  nav.style.height = "60%";
+  header.style.height = "80vh";
+  showNavItems();
+});
+
+closeIcon.addEventListener("click", () => {
+  close();
 });
